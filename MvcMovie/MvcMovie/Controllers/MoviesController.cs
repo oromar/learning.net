8386e96e6dde0274da4fe94e2b0c4b55cf3dcbc6
Oromar Lamounier Dantas Melo <oldm@cin.ufpe.br>
2015-09-18 14:43:53 -0300

    First Commit

---
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{ 
+    public class MoviesController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        //
+        // GET: /Movies/
+
+        public ViewResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        //
+        // GET: /Movies/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // GET: /Movies/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Movies/Create
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(movie);
+        }
+        
+        //
+        // GET: /Movies/Edit/5
+ 
+        public ActionResult Edit(int id = 0)
+        {
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // POST: /Movies/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        public ActionResult SearchIndex(string genre, string searchValue)
+        {
+            var genreList = new List<string>();
+
+            var genreQry =  from m in db.Movies
+                            orderby m.Genre
+                            select m.Genre;
+            genreList.AddRange(genreQry.Distinct());
+            ViewBag.genre = new SelectList(genreList);
+
+            var movies = from m in db.Movies
+                         select m;
+            if (!String.IsNullOrEmpty(searchValue))
+            {
+                movies = movies.Where(movie => movie.Title.Contains(searchValue));
+            }
+
+            if (String.IsNullOrEmpty(genre))
+            {
+                return View(movies);
+
+            }
+            else
+            {
+                return View(movies.Where(m => m.Genre == genre));
+            }
+        }
+
+
+        //
+        // GET: /Movies/Delete/5
+ 
+        public ActionResult Delete(int id = 0)
+        {
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // POST: /Movies/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id = 0)
+        {            
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID               { get; set; }
+        [Required( ErrorMessage = "Title is Required !")]
+        public string Title         { get; set; }
+        [Required(ErrorMessage = "Release Date is Required !")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "Genre is Required !")]
+        public string Genre         { get; set; }
+        [Required(ErrorMessage = "Title is Required !")]
+        [Range(1, 100, ErrorMessage = "Price must be set between $1 and $100 !")]
+        [DataType(DataType.Currency)]
+        public decimal Price        { get; set; }
+        [Required (ErrorMessage = "Rating is Required !")]
+        [StringLength(5)]
+        public string Rating        { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+
+}
